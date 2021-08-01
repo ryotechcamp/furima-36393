8fedab3ecf19d6791aafe42725d3bfc9b033c89a
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-01 13:13:31 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatabl
+
+  validates nickname, presence: true
+
+  with_options presence: true, format: { with: /^[ぁ-んァ-ヶー一-龠]+$/, message: 'first name is invalid. Input full-width characters' } do
+    validates :kanji_first_name
+  end
+  with_options presence: true, format: { with: /^[ぁ-んァ-ヶー一-龠]+$/, message: 'last name is invalid. Input full-width characters' } do
+    validates :kanji_last_name
+  end
+  with_options presence: true, format: { with: /^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/, message: 'first name kana is invalid. Input full-width characters' } do
+    validates :kana_first_name
+  end
+  with_options presence: true, format: { with: /^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/, message: 'last name kana is invalid. Input full-width characters' } do
+    validates :kana_last_name
+  end
+  validates birthday, presence: true
 
 end
