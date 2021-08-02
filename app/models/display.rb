6263974ec65ddff89a729fb0d8571a4fecf27e09
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-02 15:20:39 +0900

    バリデーション設定

--- app/models/display.rb
+++ app/models/display.rb
@@ -1,4 +1,14 @@
 class Display < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :display_name, presence: true
+  validates :instruction, presence: true
+  validates :display_name, presence: true
+  validates :genre_id, presence: true
+  validates :status_id, presence: true
+  validates :load_id, presence: true
+  validates :area_id, presence: true
+  validates :day_id, presence: true
+  validates :price, presence: true
 end
