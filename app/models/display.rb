f9465db5e2b212d952584f09f03db5da937880d1
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-02 16:44:17 +0900

    ActiveHash導入

--- app/models/display.rb
+++ app/models/display.rb
@@ -1,14 +1,23 @@
 class Display < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :status
+  belongs_to :load
+  belongs_to :area
+  belongs_to :day
+  
+
   belongs_to :user
   has_one_attached :image
 
   validates :display_name, presence: true
   validates :instruction, presence: true
   validates :display_name, presence: true
-  validates :genre_id, presence: true
-  validates :status_id, presence: true
-  validates :load_id, presence: true
-  validates :area_id, presence: true
-  validates :day_id, presence: true
-  validates :price, presence: true
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :load_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :day_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :price, numericality: { other_than: 1, message: "can't be blank" } 
 end
