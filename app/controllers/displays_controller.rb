3cf2c06a7823aa7cb042975047b69aed2fbfda93
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-03 14:42:43 +0900

    indexアクションにインスタンス変数生成

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -1,7 +1,7 @@
 class DisplaysController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    
+    @displays = Display.include(:user).order("created_at DESC")
   end
 
   def new
