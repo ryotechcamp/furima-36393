d9dbf260ac3de7fd704e76f66099effb53fe2854
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-04 13:59:21 +0900

    フォームオブジェクト保存機能追記

--- app/models/purchase_shipping_adress.rb
+++ app/models/purchase_shipping_adress.rb
@@ -1,6 +1,6 @@
 class PurchaseShipping_address
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :city, :house_number, house_name, :telephone_number, user_id, :display_id
+  attr_accessor :postal_code, :area_id, :city, :house_number, :house_name, :telephone_number, :user_id, :display_id
 
   with_option presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,5 +10,10 @@ class PurchaseShipping_address
     validates :telephone_number: {with: /\A[0-9]{10, 11}\z/, , message: 'is invalid'}
     validates :user_id
     validates :display_id
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, display_id: display_id)
+    Shipping_address.create(postal_code: postal_code, area_id: area_id, city: city, house_number: house_number, house_name: house_name, telephone_number: telephone_number, purchase_id: purchase_id)
   end
 end
