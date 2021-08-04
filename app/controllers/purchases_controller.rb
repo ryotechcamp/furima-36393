a63763f59ee957f68b4857d2fc1813b41595ae90
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-04 16:04:24 +0900

    indexアクション追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,6 @@
 class PurchasesController < ApplicationController
   def index
+    @display = Display.find(params[:display_id])
     @purchase_shipping_address = PurchaseShippingAddress.new
   end
 
