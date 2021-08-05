57a753fca0204c9e8812d780c537888e697c02a2
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-05 20:45:15 +0900

    テストコード記述

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,11 +3,11 @@ class PurchaseAddress
   attr_accessor :postal_code, :area_id, :city, :house_number, :house_name, :telephone_number, :user_id, :display_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :house_number
-    validates :telephone_number, numericality: {with: /\A[0-9]{10, 11}\z/, message: 'is invalid'}
+    validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: 'is invalid.Input only number'}
     validates :user_id
     validates :display_id
   end
---
+++ spec/models/purchase_address_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      display = FactoryBot.create(:display)
+      @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, display_id: display.id)
+      sleep 0.1
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@purchase_address).to be_valid
+      end
+      it '建物名は空でも保存できること' do
+        @purchase_address.house_name = ''
+        expect(@purchase_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it '郵便番号が空だと保存できないこと' do
+        @purchase_address.postal_code = ''
+        @purchase_address.valid?
+        #binding.pry
+        expect(@purchase_address.errors.full_messages).to include("Postal code can't be blank")
+        
+      end
+      it '郵便番号が半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @purchase_address.postal_code = '1234567'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
+      end
+      it '都道府県を選択していないと保存できないこと' do
+        @purchase_address.area_id = 1
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Area can't be blank")
+      end
+      it '市区町村が空だと保存できないこと' do
+        @purchase_address.city = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空だと保存できないこと' do
+        @purchase_address.house_number = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("House number can't be blank")
+      end
+      it '電話番号がでは保存できないこと' do
+        @purchase_address.telephone_number = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Telephone number can't be blank")
+      end
+      it '電話番号が全角では保存できないこと' do
+        @purchase_address.telephone_number = '１２３４５６７８９０'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Telephone number is invalid.Input only number")
+      end
+      it '電話番号が9桁以下では保存できないこと' do
+        @purchase_address.telephone_number = '123456789'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Telephone number is invalid.Input only number')
+      end
+      it '電話番号が12桁以上では保存できないこと' do
+        @purchase_address.telephone_number = 123456789012
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Telephone number is invalid.Input only number')
+      end
+    end
+  end
+end
