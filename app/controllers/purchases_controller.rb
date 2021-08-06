567f63938ddc77ebcc7b381b6237c45e48ef67e6
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-06 14:27:56 +0900

    環境変数導入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,19 +5,29 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @display = Display.find(params[:display_id])
+    
     @purchase_address = PurchaseAddress.new(purchase_params)
-    binding.pry
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
-      @display = Display.find(params[:display_id])
       render :index
     end
   end
 
     private
     def purchase_params
-      params.require(:purchase_address).permit(:postal_code, :area_id, :city, :house_number, :house_name, :telephone_number).merge(user_id: current_user.id, display_id: params[:display_id])
+      params.require(:purchase_address).permit(:postal_code, :area_id, :city, :house_number, :house_name, :telephone_number).merge(user_id: current_user.id, display_id: params[:display_id], token: params[:token])
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @display.price,  
+        card: purchase_params[:token],
+        currency: 'jpy' 
+      )      
     end
 end
